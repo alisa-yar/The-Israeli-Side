ef42b6cbc3c6b3f23022bfd8b9ac5b79dde13f24
Alisa <alisa.yar@icloud.com>
2023-10-14 17:56:54 +0300

    more information added, and soem style changes

--- dark.js
+++ dark.js
@@ -1,18 +1,38 @@
+// window.addEventListener("load", (event) => {
+//     const theme = localStorage.getItem('theme')
+//     if (theme == 'light') {
+//         changeTheme()
+//     }
+// })
+
+// function changeTheme() {
+//     document.body.classList.toggle("light-mode")
+//     if(document.body.classList.contains("light-mode")){
+//         document.getElementById("icon").src = "icons/moon.png";
+//         localStorage.setItem('theme', 'light')
+//     }
+//     else {
+//         document.getElementById("icon").src = "icons/sun.png";
+//         localStorage.setItem('theme', 'dark')
+//     }
+// }
+
+
 window.addEventListener("load", (event) => {
     const theme = localStorage.getItem('theme')
-    if (theme == 'light') {
+    if (theme == 'dark') {
         changeTheme()
     }
 })
 
 function changeTheme() {
-    document.body.classList.toggle("light-mode")
-    if(document.body.classList.contains("light-mode")){
-        document.getElementById("icon").src = "icons/moon.png";
-        localStorage.setItem('theme', 'light')
-    }
-    else {
+    document.body.classList.toggle("dark-mode")
+    if(document.body.classList.contains("dark-mode")){
         document.getElementById("icon").src = "icons/sun.png";
         localStorage.setItem('theme', 'dark')
+    }
+    else {
+        document.getElementById("icon").src = "icons/moon.png";
+        localStorage.setItem('theme', 'light')
     }
 }
